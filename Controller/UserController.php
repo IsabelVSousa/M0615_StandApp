d3ac95006f85f653e7ef05188ff571b6418a6ea9
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-03-20 09:01:18 +0100

    #8 conect error message added and utf-8

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -3,56 +3,67 @@ session_start();
 
 // si el metodo es post creo el usuario
 //porque no es un isset??
-if($_SESSION["REQUEST_METHOD"] == "POST"){
+if ($_SESSION["REQUEST_METHOD"] == "POST") {
     $userController = new UserController();
 }
 
 // que tipo de post es y derivar a su funcion
-if(isset($_POST["login"])){
+if (isset($_POST["login"])) {
     echo "<p>Login click</p>";
     $userController->login();
 }
 
-if(isset($_POST["logout"])){
+if (isset($_POST["logout"])) {
     echo "<p>Logout click</p>";
     $userController->lougout();
 }
 
-if(isset($_POST["register"])){
+if (isset($_POST["register"])) {
     echo "<p>Register click</p>";
     $userController->register();
 }
 
 
-class UserController{
-    
+class UserController
+{
+
     //hay que poner aqui algun atributo??
     private $conn;
 
     //aqui tendria que ir el constructor   
     public function __construct()
     {
-        $servername = "localhost"; 
+        $servername = "localhost";
         $username = "root";
-    
+
         //falta asignar la password
         $password = "";
         $dbname = "MF_StandApp";
 
         $conexion = new mysqli("localhost", "root", "", "MF_StandApp");
+
+        //dentro del constructor??
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
+        }
+
+        echo "Conexión exitosa";
+
+        $conexion->set_charset("utf8mb4");
     }
 
-    public function login():void {
+    public function login(): void
+    {
         echo "login";
     }
-    
-    public function lougout():void {
+
+    public function lougout(): void
+    {
         echo "logout";
     }
-    
-    public function register():void {
+
+    public function register(): void
+    {
         echo "register";
     }
-    
 }
-?>
